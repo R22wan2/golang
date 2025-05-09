4f5fa13fd23bf6a5216857bf7750ef73d19a05d2
R22wan2 <riswanmohamed160@gmail.com>
2025-05-10 00:17:59 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/R22wan2/golang
+
+go 1.24.0
---
+++ god.go
@@ -0,0 +1,7 @@
+package god
+
+import "fmt"
+
+func Get_User_Print(name string) {
+	fmt.Printf("the name you gave that is %s", name)
+}
